6cddb3fd2d5a9d94fbdf4f2f311dae2c9cd6115d
Sumit0673 <gsumitg673@gmail.com>
2026-08-01 11:25:55 +0530

    double enclosure

--- auto-spec/auto_spec/prompts/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Prompts module initialization."""
-
-from auto_spec.prompts.property_gpt import (
-    format_property_gpt_prompt,
-    PROPERTY_GPT_SYSTEM_PROMPT
-)
-
-__all__ = ["format_property_gpt_prompt", "PROPERTY_GPT_SYSTEM_PROMPT"]
